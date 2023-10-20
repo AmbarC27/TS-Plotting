17d33818b03a67c6117e03b8c9a46b2c98642703
AmbarC27 <ambarchakra@gmail.com>
2023-10-20 19:09:33 -0400

    Cleaned variables

--- src/Barchart.tsx
+++ src/Barchart.tsx
@@ -8,35 +8,35 @@ interface Props {
   barColor: string;
 }
 
-const SVG_WIDTH = 600;
-const SVG_HEIGHT = 300;
-const BAR_WIDTH = 40;
-const LABEL_PADDING = 10;
+const width = 600;
+const height = 300;
+const bar_width = 40;
+const padding = 10;
 
 const BarChart: React.FC<Props> = ({ numbers, labels, direction, barColor }) => {
   const maxValue = Math.max(...numbers);
   const totalBars = numbers.length;
 
   const bars = numbers.map((number, index) => {
-    const barHeight = (number / maxValue) * SVG_HEIGHT;
-    const barX = (SVG_WIDTH / totalBars) * index;
-    const barY = SVG_HEIGHT - barHeight;
+    const barHeight = (number / maxValue) * height;
+    const barX = (width / totalBars) * index;
+    const barY = height - barHeight;
     
-    let labelX = barX + BAR_WIDTH / 2;
-    let labelY = direction === 'vertical' ? SVG_HEIGHT + 20 : barY + BAR_WIDTH / 2;
+    let labelX = barX + bar_width / 2;
+    let labelY = direction === 'vertical' ? height + 20 : barY + bar_width / 2;
 
     if (direction === 'horizontal') {
-      labelX = LABEL_PADDING;
-      labelY = barY + BAR_WIDTH / 2; 
+      labelX = padding;
+      labelY = barY + bar_width / 2; 
     }
 
     return (
       <g key={index}>
         {(() => {
           if (direction === 'vertical') {
-            return <rect x={barX} y={barY} width={BAR_WIDTH} height={barHeight} fill={barColor} />;
+            return <rect x={barX} y={barY} width={bar_width} height={barHeight} fill={barColor} />;
           } else if (direction === 'horizontal') {
-            return <rect x={0} y={barY} width={barHeight} height={BAR_WIDTH} fill={barColor} />;
+            return <rect x={0} y={barY} width={barHeight} height={bar_width} fill={barColor} />;
           }
         })()}
         <text x={labelX} y={labelY} textAnchor="end" alignmentBaseline="middle" fontSize="12">
@@ -48,7 +48,7 @@ const BarChart: React.FC<Props> = ({ numbers, labels, direction, barColor }) => 
 
   return (
     <div className="card">
-      <svg width={SVG_WIDTH} height={SVG_HEIGHT + 30}>
+      <svg width={width} height={height + 30}>
         {bars}
       </svg>
     </div>
