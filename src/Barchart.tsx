134c6e41e56a4304b6d60a99ebe75259b2451243
AmbarC27 <ambarchakra@gmail.com>
2023-10-20 20:05:51 -0400

    Added hovering effect

--- src/Barchart.tsx
+++ src/Barchart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Barchart.css';
 
 interface Props {
@@ -14,11 +14,13 @@ const bar_width = 40;
 const padding = 10;
 
 const BarChart: React.FC<Props> = ({ numbers, labels, direction, barColor }) => {
+  const [hoveredBar, setHoveredBar] = useState<number | null>(null);
+  
   const maxValue = Math.max(...numbers);
   const totalBars = numbers.length;
 
   const bars = numbers.map((number, index) => {
-    const barHeight = (number / maxValue) * height;
+    const barHeight = (number / maxValue) * height * 0.9;
     const barX = (width / totalBars) * index;
     const barY = height - barHeight;
     
@@ -34,14 +36,46 @@ const BarChart: React.FC<Props> = ({ numbers, labels, direction, barColor }) => 
       <g key={index}>
         {(() => {
           if (direction === 'vertical') {
-            return <rect x={barX} y={barY} width={bar_width} height={barHeight} fill={barColor} />;
+            return (
+              <rect 
+                className="bar"
+                x={barX} 
+                y={barY} 
+                width={bar_width} 
+                height={barHeight} 
+                fill={barColor} 
+                onMouseEnter={() => setHoveredBar(index)}
+                onMouseLeave={() => setHoveredBar(null)}
+              />
+            );
           } else if (direction === 'horizontal') {
-            return <rect x={0} y={barY} width={barHeight} height={bar_width} fill={barColor} />;
+            return (
+              <rect 
+                className="bar"
+                x={0} 
+                y={barY} 
+                width={barHeight} 
+                height={bar_width} 
+                fill={barColor} 
+                onMouseEnter={() => setHoveredBar(index)}
+                onMouseLeave={() => setHoveredBar(null)}
+              />
+            );
           }
         })()}
         <text x={labelX} y={labelY} textAnchor="end" alignmentBaseline="middle" fontSize="12">
           {labels[index]}
         </text>
+        {hoveredBar === index && (
+          <text 
+            x={barX + bar_width / 2} 
+            y={barY - 5} 
+            textAnchor="middle"
+            fontSize="12"
+          >
+            {number}
+          </text>
+        )}
       </g>
     );
   });
